248cbbf119ce1d1d145f5ffe0e14bacdddfafd3a
Alice Duong <alic0579@edu.eal.dk>
2018-09-25 12:03:40 +0200

    Models

---
+++ WelfareDenmarkLiveMap/Models/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelfareDenmarkLiveMap.Models
+{
+    public class Session
+    {
+    }
+}
