1b43cc95547ccbacbfbfbcd5ffd56fe7cc515af3
Mads Nielsen <mads9106@edu.eal.dk>
2018-10-23 13:19:41 +0200

    Added PatientGenerator

--- WelfareDenmarkLiveMap/Controllers/PatientController.cs
+++ WelfareDenmarkLiveMap/Controllers/PatientController.cs
@@ -26,15 +26,85 @@ namespace WelfareDenmarkLiveMap.Controllers
         [Route("create")]
         public IActionResult Create()
         {
+            Random r = new Random();
+            List<string> firstnames = new List<string>
+            {
+                "Mads",
+                "André",
+                "Alice",
+                "Jens",
+                "Peter",
+                "Mikael",
+                "Lars",
+                "Bo",
+                "Hans",
+                "Emil",
+                "Ole",
+                "Allan",
+                "Henrik",
+                "Carsten",
+                "Jonas",
+                "Mikki",
+                "Martin",
+                "Simon",
+                "Erik",
+                "Lars",
+                "Alex",
+                "Søren",
+                "Mia",
+                "Charlotte",
+                "Kida",
+                "Lilje",
+                "Rikke",
+                "Lisa",
+                "Else",
+                "Birthe",
+                "Stine",
+                "Linda",
+                "Malene",
+                "Louise",
+                "Mathilde",
+                "Mette",
+                "Irene",
+                "Gitte",
+                "Henriette",
+                "Kirsten",
+                "Gudrun",
+                "Line",
+                "Helene",
+                "Agnete"
+            };
+            List<string> lastnames = new List<string>
+            {
+                "Nielsen",
+                "Hansen",
+                "Jensen",
+                "Thomsen",
+                "Larsen",
+                "Christensen",
+                "Møller",
+                "Busch",
+                "Beck",
+                "Gollubits",
+                "Møller",
+                "Robert",
+                "Damgaard",
+                "Daugaard",
+                "Henningsen",
+                "Wilhelmsen",
+                "Rasmussen",
+                "Mortensen"
+            };
             if (!ModelState.IsValid)
                 return View();
-            Patient patient = new Patient();
-
-            patient.Name = "Mads";
-            patient.Sessions = null;
-            patient.County = null;
-
-            _db.Patients.Add(patient);
+            for (int i = 0; i < 100; i++)
+            {
+                Patient patient = new Patient();
+                patient.Name = $"{firstnames[r.Next(0, firstnames.Count)]} {lastnames[r.Next(0, lastnames.Count)]}";
+                patient.Sessions = null;
+                patient.County = null;
+                _db.Patients.Add(patient);
+            }
             _db.SaveChanges();
 
             return RedirectToAction("map", "map", new
