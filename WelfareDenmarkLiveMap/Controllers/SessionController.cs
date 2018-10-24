8b2988998d6bcc9ddf79aae920f63927bd9ece6c
mads221h <mads221h@edu.eal.dk>
2018-10-24 10:29:19 +0200

    Sessioncontroller "Create" route added

--- WelfareDenmarkLiveMap/Controllers/SessionController.cs
+++ WelfareDenmarkLiveMap/Controllers/SessionController.cs
@@ -7,6 +7,7 @@ using WelfareDenmarkLiveMap.Models;
 
 namespace WelfareDenmarkLiveMap.Controllers
 {
+    [Route("session")]
     public class SessionController : Controller
     {
         private readonly DataContext _db;
@@ -15,13 +16,42 @@ namespace WelfareDenmarkLiveMap.Controllers
         {
             _db = db;
         }
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
-
+        [Route("Create")]
         public IActionResult Create()
         {
+            var patients = _db.Patients.ToList();
+            var exercises = _db.Exercise.ToList();
+            Random random = new Random();
+            var maxExercises = random.Next(6, 20);
+            var Sessions = new List<Session>();
+            var completedExercises = new List<Exercise>();
+            DateTime dateTime = new DateTime();
+            
+
+            for (int i = 0; i < maxExercises; i++)
+            {
+                var randomExercise = random.Next(1, 20);            
+                completedExercises.Add(exercises[randomExercise]);
+            }
+            
+                foreach (var Patient in  patients) {
+                var session = new Session
+                {
+                    CompletionRate = random.Next(10, 100),
+                    Time = dateTime.Date,
+                    Patient = Patient,
+                    Exercises = completedExercises
+
+                };
+                Sessions.Add(session);
+            };
+            _db.AddRange(Sessions);
+            _db.SaveChanges();
             return View();
         }
     }
