6a3caba6e7677708ceb41debeaf15d562583faad
mads221h <mads221h@edu.eal.dk>
2018-10-26 11:03:40 +0200

    Unfinished  data action in mapcontroller

--- WelfareDenmarkLiveMap/Controllers/MapController.cs
+++ WelfareDenmarkLiveMap/Controllers/MapController.cs
@@ -11,10 +11,45 @@ namespace WelfareDenmarkLiveMap.Controllers
     [Route("map")]
     public class MapController : Controller
     {
+        private readonly DataContext _db;
         [Route("")]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost, Route("data")]
+        public IActionResult Data(string countyID)
+        {
+            return Content(countyID);
+            //var counties = _db.County.ToList();
+            //var patients = _db.Patients.ToList();
+            //var sessions = _db.Session.ToList();
+            //var hoverCounty = new County();
+            //var hoverPatient = new List<Patient>();
+            //var hoverSessions = new List<Session>();
+            //foreach (var county in counties)
+            //{
+            //    if(county.ID == countyID)
+            //    {
+            //        hoverCounty = county;
+            //    }
+            //}
+            //foreach (var patient in patients)
+            //{
+            //    if(patient.County == hoverCounty)
+            //    {
+            //        hoverPatient.Add(patient);
+            //            foreach (var session in sessions)
+            //            {
+            //               if (session.Patient == patient)
+            //                 {
+            //                    sessions.Add
+            //                 }
+            //            }
+            //    }
+            //}
+            //return View();
+        }
+
     }
 }
